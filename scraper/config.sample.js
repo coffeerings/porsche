d6e11c8741d954ab417fa64035319eac431303d0
John Walker <johnwalker@Johns-Air.default>
2016-01-03 17:10:39 +0000

    Sample config

---
+++ scraper/config.sample.js
@@ -0,0 +1,10 @@
+var config = {};
+config.mysql = {};
+
+config.mysql.host = 'localhost';
+config.mysql.port = 3306;
+config.mysql.user = 'root';
+config.mysql.password = 'root';
+config.mysql.database = 'cars';
+
+module.exports = config;
