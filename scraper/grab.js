7b07c7fff5d27057180d3ba954bd105e3bd0145a
John walker <johnwalker@Johns-MacBook-Air.local>
2021-07-06 22:06:31 +0800

    Finally started

--- scraper/grab.js
+++ scraper/grab.js
@@ -1,160 +1,51 @@
-var jsdom = require('jsdom'), 
-    mysql = require('mysql'),
-    config = require('./config'),
-    fs = require("fs");
+const fs = require('fs');
+const got = require('got');
+const jsdom = require("jsdom");
+const url = require('url');
+const { JSDOM } = jsdom;
 
-var jquery = fs.readFileSync(__dirname + "/node_modules/jquery/dist/jquery.min.js", "utf-8");
+function fetch(page){
+  got(page).then(response => {
+    const model = url.parse(page, true).query.M;
 
-function getQueryParams(qs) {
-    qs = qs.split("+").join(" ");
-    var params = {}, tokens,
-        re = /[?&]?([^=]+)=([^&]*)/g;
-    while (tokens = re.exec(qs)) {
-        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
-    }
-    return params;
-}
+    const dom = new JSDOM(response.body);
+    dom.window.document.querySelectorAll('#search-results .result-contain').forEach(ad => {
+      if((cardom = ad.querySelector('.listing-utils div')) !== null){
+        
+        title = ad.querySelector('.listing-headline h3').innerHTML;
+        year = title.substr(title.length - 6).replace("(","").replace(")","");
+        details = ad.querySelectorAll(('.listing-info .specs li'))
+        
+        car = {
+          ref: cardom.id,
+          title: title,
+          year: year.toString(),
+          link: ad.querySelector('.listing-headline a').href,
+          price: parseInt(ad.querySelector('.listing-headline .price span').innerHTML.replace("£","").replace(",","").toString()),
+          miles: parseInt(details[0].textContent.replace(",","").replace("miles","").replaceAll("\n","").replaceAll(" ","").toString()),
+          location: ad.querySelector('.listing-dealer .location').textContent.replaceAll("\n","").replaceAll(" ",""),
+          dealer: ad.querySelector('.listing-dealer a').href,
+          model: model,
+          source: 'pistonheads'
+        };
 
-function get_cars(url, callback){
-    console.log("Fetching: " + url);
-    
-    var query_param = getQueryParams(url);
-    var callback = callback;
-    
-    jsdom.env({
-        url: url,
-        src: [jquery],
-        done: function(err, window){
-            var $ = window.$;
-            var cars = [];
-
-            if(query_param.hasOwnProperty("M")){
-                source = "pistonheads";
-                model = query_param.M;
-
-                if($("#search-results .result-contain").length){
-                    $("#search-results .result-contain").each(function( index ) {
-                        if(typeof $(this).find('.listing-utils div').attr('id') !== "undefined"){
-                            title = $(this).find('.listing-headline h3').text();
-                            year = title.substr(title.length - 6).replace("(","").replace(")","");
-
-                            car = {
-                                ref: $(this).find('.listing-utils div').attr('id').toString(),
-                                title: title,
-                                year: year.toString(),
-                                link: $(this).find('.listing-headline a').attr('href'),
-                                price: parseInt($(this).find('.listing-headline .price span').text().replace("£","").replace(",","")).toString(),
-                                miles: parseInt($.trim($(this).find('.listing-info .specs li:eq(0)').text()).replace(",","")).toString(),
-                                location: $.trim($(this).find('.listing-dealer .location').text()),
-                                dealer: $.trim($(this).find('.listing-dealer .name').text()),
-                                source : source,
-                                model : model
-                            };
-                            cars.push(car);
-                        }
-                    });
-                    callback(cars, url);
-                }
-            }
-        }
+        console.log(car)
+      };   
     });
+  }).catch(err => {
+      console.log(err);
+  });
 }
 
-function insert_cars(cars, url){
-    console.log("Inserting: " + cars.length + " cars against the url: " + url);
+var pages = [
+  'https://www.pistonheads.com/classifieds?Category=used-cars&M=2898&ResultsPerPage=10',
+  'https://www.pistonheads.com/classifieds?Category=used-cars&M=2898&Page=2&ResultsPerPage=10',
+  'https://www.pistonheads.com/classifieds?Category=used-cars&M=525&ResultsPerPage=10'
+];
 
-    var cars = cars;
-
-    var pool  = mysql.createPool({
-        connectionLimit : 50,
-        connectTimeout  : 60 * 60 * 1000,
-        aquireTimeout   : 60 * 60 * 1000,
-        timeout         : 60 * 60 * 1000,
-        waitForConnections : true,
-        user     : config.mysql.user,
-        password : config.mysql.password,
-        database : config.mysql.database,
-        socketPath : config.mysql.socketPath
-    });
-
-    pool.getConnection(function(err, connection) {
-        if (err) throw err;
-        connection.beginTransaction(function(err) {
-            if (err) throw err;
-            for(i = 0; i < cars.length; i++) {
-                connection.query('INSERT INTO cars SET ?', cars[i], function(err, result) {
-                    if(err){
-                        return connection.rollback(function() {
-                            throw error;
-                        });
-                        return;
-                    }
-                });
-                console.log("Inserted: " + cars[i].ref + ": " + cars[i].title + " - " + cars[i].price);
-            }
-            connection.commit(function(err) {
-                if (err) {
-                  return connection.rollback(function() {
-                    throw err;
-                  });
-                }
-                console.log('success!');
-            });
-        });
-    });
-}
-
-// Porsche 997/991 models with most variants. M3, R8, RS4 and GTR also added 
-var urls = [
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=2898&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=2898&Page=2&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=525&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=525&Page=2&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=249&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=249&Page=2&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=249&Page=3&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=222&M=1161&M=1311&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=222&M=1161&M=1311&Page=2&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=222&M=1161&M=1311&Page=3&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=301&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=301&Page=2&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=315&M=321&M=1036&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=315&M=321&M=1036&Page=2&ResultsPerPage=100',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=1047&Page=1&ResultsPerPage=100&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=1047&Page=2&ResultsPerPage=100&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=506&Page=1&ResultsPerPage=100&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=506&Page=2&ResultsPerPage=100&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=506&Page=3&ResultsPerPage=100&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=506&Page=4&ResultsPerPage=100&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=506&Page=5&ResultsPerPage=100&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=506&Page=6&ResultsPerPage=100&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=1049&Page=1&ResultsPerPage=100&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=1048&Page=1&ResultsPerPage=100&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=1400&Page=1&ResultsPerPage=100&SortOptions=ModifiedDate&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=1400&Page=2&ResultsPerPage=100&SortOptions=ModifiedDate&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=1400&Page=3&ResultsPerPage=100&SortOptions=ModifiedDate&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=1400&Page=4&ResultsPerPage=100&SortOptions=ModifiedDate&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=2502&Page=1&ResultsPerPage=100&SortOptions=ModifiedDate&isExperiment=True',
-    'http://www.pistonheads.com/classifieds?Category=used-cars&M=2501&Page=1&ResultsPerPage=100&SortOptions=ModifiedDate&isExperiment=True',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=234&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=234&Page=2&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=218&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=1162&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=516&ResultsPerPage=100&SortOptions=PriceLowToHigh',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=516&Page=2&ResultsPerPage=100&SortOptions=PriceLowToHigh',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=516&Page=3&ResultsPerPage=100&SortOptions=PriceLowToHigh',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=2913&ResultsPerPage=100',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=1538&ResultsPerPage=100&SortOptions=PriceLowToHigh',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=521&ResultsPerPage=100&SortOptions=PriceLowToHigh',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=521&Page=2&ResultsPerPage=100&SortOptions=PriceLowToHigh',
-    'https://www.pistonheads.com/classifieds?Category=used-cars&M=3039&ResultsPerPage=100&SortOptions=PriceLowToHigh'
- ];
-
-// loop through urls with a 30 sec break between each call to scrape the site
-for(i = 0; i < urls.length; i++) { 
-    (function(i){
-        setTimeout(function(){
-            get_cars(urls[i], insert_cars);
-        }, 30000 * i);
-    }(i));
-}
+(async function loop() {
+  for (let i = 0; i < pages.length; i++) {
+    await new Promise(resolve => setTimeout(resolve, Math.random() * 5000));
+    fetch(pages[i]);
+  }
+})();
